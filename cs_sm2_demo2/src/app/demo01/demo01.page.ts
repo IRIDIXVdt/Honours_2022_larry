2b7a8fc43db242465fb09d928607fbc322c67765
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2022-06-01 19:34:25 +0800

    demo 1 implemented

--- cs_sm2_demo2/src/app/demo01/demo01.page.ts
+++ cs_sm2_demo2/src/app/demo01/demo01.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { questionList } from '../shared/data/questionList';
 @Component({
   selector: 'app-demo01',
   templateUrl: './demo01.page.html',
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Demo01Page implements OnInit {
 
-  constructor() { }
+  qList = questionList;
+  index: number;
+  displayAnswer: boolean;
+  sessionEnd: boolean;
+  constructor() {
+    console.log(this.qList);
+    this.index = 0;
+    this.displayAnswer = false;
+  }
 
   ngOnInit() {
+  }
+
+  check() {
+    this.displayAnswer = true;
+  }
+
+  answer(answer: number) {
+    if (this.index + 2 > this.qList.length) {
+      this.sessionEnd = true;
+    } else {
+      this.index++;
+      this.displayAnswer = false;
+      console.log('the user answered', answer);
+    }
+
   }
 
 }
