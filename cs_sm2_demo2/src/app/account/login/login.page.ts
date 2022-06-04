837556ea3befc95fc72d6d90e3708a7ecd692509
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2022-06-04 17:23:05 +0800

    login page form

--- cs_sm2_demo2/src/app/account/login/login.page.ts
+++ cs_sm2_demo2/src/app/account/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  public errorMessages = {
+    email: [
+      {
+        type: 'required', message: 'Email is required.'
+      },
+      {
+        type: 'pattern', message: 'Please enter a valid email address.'
+      }
+    ],
+    password: [
+      {
+        type: 'required', message: 'Password is required.'
+      },
+      {
+        type: 'minlength', message: 'Your password should be more than 6 character.'
+      },
+      {
+        type: 'maxlength', message: 'Your password should be less than 20 character.'
+      },
+    ]
   }
 
+  signInForm: FormGroup;
+  constructor(
+    // public authService: AuthService
+  ) {
+    this.signInForm = new FormGroup({
+      formEmail: new FormControl('', [
+        Validators.required,
+        Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+      formPassword: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(20),
+      ])
+    });
+  }
+
+  ngOnInit() {}
 }
