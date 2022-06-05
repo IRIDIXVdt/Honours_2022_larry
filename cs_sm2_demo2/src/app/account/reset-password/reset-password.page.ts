625aa2ec07611705f3e2a771aff1462131c9e95e
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2022-06-05 20:49:33 +0800

    reset password feature implementation

--- cs_sm2_demo2/src/app/account/reset-password/reset-password.page.ts
+++ cs_sm2_demo2/src/app/account/reset-password/reset-password.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm, FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
+import { AuthService } from '../../shared/service/auth.service';
 
 @Component({
   selector: 'app-reset-password',
@@ -7,9 +10,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResetPasswordPage implements OnInit {
 
-  constructor() { }
+  public errorMessages = {
+    email: [
+      {
+        type: 'required', message: 'Email is required.'
+      },
+      {
+        type: 'pattern', message: 'Please enter a valid email address.'
+      }
+    ]
+  }
 
-  ngOnInit() {
+  resetForm: FormGroup;
+  constructor(public authService: AuthService, public alertController: AlertController,) {
+    this.resetForm = new FormGroup({
+      formEmail: new FormControl('', [
+        Validators.required,
+        Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")])
+    });
+  }
+
+  ngOnInit() {}
+
+  timeLeft: number;
+  interval;
+  block: boolean = false;
+
+  async resetPassword(email) {
+    if (this.authService.isLogin()) {
+      const alert = await this.alertController.create({
+        cssClass: 'my-custom-class',
+        message: 'For security reasons, you will be sign-out. Do you want to continue?',
+        buttons: ['Cancel', 'Yes']
+      });
+      await alert.present();
+      const { role } = await alert.onDidDismiss();
+      if (role == "cancel") {
+        console.log("cancel!");
+      } else {
+        this.startTimer()
+        this.authService.ForgotPassword(email);
+      }
+    } else {
+      this.startTimer()
+      this.authService.ForgotPassword(email);
+    }
+  }
+
+  startTimer() {
+    console.log('start')
+    this.timeLeft = 20;
+    this.interval = setInterval(() => {
+      if (this.timeLeft > 0) {
+        this.block = true;
+        this.timeLeft = this.timeLeft - 1;
+
+      } else {
+        this.block = false;
+        clearInterval(this.interval);
+      }
+    }, 1000)
   }
 
 }
