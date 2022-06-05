8e8dbae53c264f0ca6122755eaef854e036ebda4
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2022-06-05 20:58:13 +0800

    email verify feature implementation

--- cs_sm2_demo2/src/app/account/email-verify/email-verify.page.ts
+++ cs_sm2_demo2/src/app/account/email-verify/email-verify.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../shared/service/auth.service';
 
 @Component({
   selector: 'app-email-verify',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./email-verify.page.scss'],
 })
 export class EmailVerifyPage implements OnInit {
+  constructor( public authService: AuthService) { this.startTimer()}
+  ngOnInit() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  timeLeft: number = 60;
+  interval;
+  block:boolean =true;
+  resetTimeLeft(){
+    this.timeLeft =60;
   }
 
+  startTimer() {
+    return this.interval = setInterval(() => {
+      if(this.timeLeft > 0) {
+        this.block = true;
+        this.timeLeft= this.timeLeft-1;
+      } else {       
+        this.block = false;
+        clearInterval(this.interval);
+        return false;
+      }
+    },1000)
+  }
 }
