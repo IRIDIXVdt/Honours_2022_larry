2061da471f89453de484b5a91a3f746e202857a4
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2022-06-10 20:50:49 +0800

    Session Manage HTML

--- cs_sm2_demo2/src/app/admin/session/session.page.ts
+++ cs_sm2_demo2/src/app/admin/session/session.page.ts
@@ -6,7 +6,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./session.page.scss'],
 })
 export class SessionPage implements OnInit {
+  cCode: string = 'A';
+  // cList:
+  cList = [
+    {
+      cValue: 'A',
+      cName: 'All'
+    },
+    {
+      cValue: 'COSC111',
+      cName: 'COSC 111'
+    },
+    {
+      cValue: 'COSC404',
+      cName: 'COSC 404'
+    },
+    {
+      cValue: 'COSC304',
+      cName: 'COSC 304'
+    }
+  ]
 
+  sList = [
+    {
+      sValue: 'COSC304',
+      sTime: '2022WT1',
+      sNumber: '001'
+    },
+    {
+      sValue: 'COSC304',
+      sTime: '2022WT1',
+      sNumber: '002'
+    },
+    {
+      sValue: 'COSC304',
+      sTime: '2021ST1',
+      sNumber: '001'
+    },
+    {
+      sValue: 'COSC304',
+      sTime: '2021ST2',
+      sNumber: '001'
+    },
+  ]
   constructor() { }
 
   ngOnInit() {
