7d89512c43289d6e1c66f21eca1671ffb3ee6118
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2022-06-10 17:51:46 +0800

    app split pane bug fix

--- cs_sm2_demo2/src/app/app.component.ts
+++ cs_sm2_demo2/src/app/app.component.ts
@@ -10,8 +10,8 @@ export class AppComponent {
     { title: 'Account', url: '/tabs/account', icon: 'cog' },
     { title: 'CALL: Spaced Repetition Software Demo', url: '/tabs/demo01', icon: 'clipboard' },
     { title: 'New Demo', url: '/tabs/demo02', icon: 'clipboard' },
-    { title: 'Add', url: '/tabs/add', icon: 'add-circle' },
-    { title: 'Browse', url: '/tabs/browse', icon: 'albums' },
+    // { title: 'Add', url: '/tabs/add', icon: 'add-circle' },
+    // { title: 'Browse', url: '/tabs/browse', icon: 'albums' },
   ];
   constructor() {
 
