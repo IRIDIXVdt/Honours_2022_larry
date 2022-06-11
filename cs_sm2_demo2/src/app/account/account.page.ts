2ac1cfcf7ef11ebcf57f50f24c8f13b48fe882d8
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2022-06-11 23:59:34 +0800

    Normal User Session Register Page Front End

--- cs_sm2_demo2/src/app/account/account.page.ts
+++ cs_sm2_demo2/src/app/account/account.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../shared/service/auth.service';
+import { DatabaseService } from '../shared/service/database.service';
 import { FirebaseService } from '../shared/service/firebase.service';
 
 @Component({
@@ -8,18 +9,26 @@ import { FirebaseService } from '../shared/service/firebase.service';
   styleUrls: ['./account.page.scss'],
 })
 export class AccountPage implements OnInit {
-
+  sessionId = '';
+  sList: any;
   constructor(
     public aus: AuthService,
     public afs: FirebaseService,
+    public das: DatabaseService,
   ) {
-    // afs.getDocument('users', '0PBmtDa6DIM2igdjKJv54GtWt1o2').subscribe(v=>{
-    //   console.log(v);
-    //   const newD = {
-    //     email: v.data()['email'],
-    //   }
-    //   console.log(newD);
-    // })
+    if (!this.aus.isAdmin()) {//normal user
+      //load sList
+      this.fetchSession();
+    }
+    
+  }
+
+  fetchSession() {
+    this.sList = null;
+    this.das.getSessionData('All').then(v => {
+      this.sList = v;
+      console.log(this.sList);
+    });
   }
 
   ngOnInit() {
