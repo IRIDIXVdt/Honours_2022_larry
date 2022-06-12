0ea0539cd29388c75e893609915b4e012cf66c06
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2022-06-12 14:36:13 +0800

    Update local-storage.service.ts

--- cs_sm2_demo2/src/app/shared/service/local-storage.service.ts
+++ cs_sm2_demo2/src/app/shared/service/local-storage.service.ts
@@ -6,4 +6,10 @@ import { Injectable } from '@angular/core';
 export class LocalStorageService {
 
   constructor() { }
+
+  isLogin() { //return true if has logged in
+    // console.log(JSON.parse(localStorage.getItem('user')));
+    return JSON.parse(localStorage.getItem('user')) != null;
+  }
+  
 }
