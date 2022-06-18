bcca40d79fd2e4be8d6092ce7dacfc36e1a4f3ef
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2022-06-18 18:29:37 +0800

    question store local update

--- cs_sm2_demo2/src/app/demo02/demo02.page.ts
+++ cs_sm2_demo2/src/app/demo02/demo02.page.ts
@@ -29,7 +29,11 @@ export class Demo02Page implements OnInit {
   ) {
     //loadQuestionList
     dab.getQuestionData().then(v => {
+      //then initialize all the question as unanswered
       this.qList = (v as any[]);
+      for (let i = 0; i < this.qList.length; i++) {
+        this.qList[i].answered = false;
+      }
       //then initialize index
       this.index = 0;
       this.displayAnswer = false;
@@ -39,8 +43,8 @@ export class Demo02Page implements OnInit {
 
   ngOnInit() {
   }
-  updateEnableDisplayAnswer() {
 
+  updateEnableDisplayAnswer() {
     console.log(this.qList[this.index].qType);
     if (this.qList[this.index].qType === 'ba') {
       console.log('this is a basic type question')
@@ -49,6 +53,9 @@ export class Demo02Page implements OnInit {
       this.disableDisplayAnswer = true;
     }
   }
+
+
+
   enableDisplay() {
     // console.log('trigger');
     this.disableDisplayAnswer = false;
@@ -58,14 +65,39 @@ export class Demo02Page implements OnInit {
     this.displayAnswer = true;
   }
 
+  saveAndStore
+
   answer(answer: number) {
     if (this.index + 2 > this.qList.length) {
       this.sessionEnd = true;
-    } else {
+      //end this session
+      //then upload everything in this session to database
+    } else {//handle the responsive
+      /*
+      if user is first time answering this question
+        if quality is easy, store info and end this right away
+        if quality is not easy, store info and repeat this three time
+      or if the user is attempting this question already
+        if quality is poor or repeat, repeat it once
+        if quality is good or easy, check if there are repeat time left
+          if no repeat time left, store info end this
+      */
+      if(!this.qList[this.index].answered){
+        if(answer==3){//quality easy
+
+        }else{
+          this.qList[this.index];
+        }
+      }else{
+
+      }
+
+     
       this.index++;
       this.displayAnswer = false;
       console.log('the user answered', answer);
       this.updateEnableDisplayAnswer();
+
     }
     this.userCode = '';
     this.userMulti = '';
