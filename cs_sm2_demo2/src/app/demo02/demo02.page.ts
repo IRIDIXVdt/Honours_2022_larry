20b0a23a6aa7f4cbdbaa11f824e1b8361ed9874e
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2022-06-19 15:10:50 +0800

    session end condition bug fix

--- cs_sm2_demo2/src/app/demo02/demo02.page.ts
+++ cs_sm2_demo2/src/app/demo02/demo02.page.ts
@@ -46,13 +46,14 @@ export class Demo02Page implements OnInit {
   }
 
   updateEnableDisplayAnswer() {
-    console.log(this.qList[0].qType);
-    if (this.qList[0].qType === 'ba') {
-      // console.log('this is a basic type question')
-      this.disableDisplayAnswer = false;
-    } else {
-      this.disableDisplayAnswer = true;
-    }
+    // console.log(this.qList[0].qType);
+    if (this.qList.length > 0)
+      if (this.qList[0].qType === 'ba') {
+        // console.log('this is a basic type question')
+        this.disableDisplayAnswer = false;
+      } else {
+        this.disableDisplayAnswer = true;
+      }
   }
 
 
@@ -72,61 +73,62 @@ export class Demo02Page implements OnInit {
   }
 
   answer(answer: number) {
-    if (this.qList.length == 1) {
+    //handle the responsive: repeat some of the question
+    /*
+    if user is first time answering this question
+      if quality is easy, store info and end this right away
+      if quality is not easy, store info and repeat this three time
+    or if the user is attempting this question already
+      if quality is poor or repeat, repeat it once
+      if quality is good or easy, check if there are repeat time left
+        if no repeat time left, store info end this
+    */
+    var currentItem = this.qList.shift();//get the very first item
+    if (!currentItem.answered) {
+      currentItem.answered = true;//update currenItem answer
+      if (answer == 3) {//quality easy
+        //remove item from the list and
+        //to do: store the item
+      } else {
+        currentItem.level = answer;//this is the level
+        currentItem.repeatTime = 3;//repeat it for three times
+        this.qList.splice(Math.round(this.qList.length / 2), 0, currentItem);//insert this item to the middle
+      }
+    } else {
+      if (answer == 0 || answer == 1) {
+        //don't change the repeat time
+        console.log('respond poor quality, repeat question')
+        this.qList.splice(Math.round(this.qList.length / 2), 0, currentItem);
+      } else {
+        if (currentItem.repeatTime == 1) {
+          //to do: store the item
+          //addition requirement: if the length gets to 0 end this session
+        } else {
+          console.log('respond good quality, minus repeat time by 1')
+          const repeatTime = currentItem.repeatTime;
+          currentItem.repeatTime = repeatTime - 1;
+          this.qList.splice(Math.round(this.qList.length / 2), 0, currentItem);
+        }
+      }
+    }
+
+    if (this.qList.length == 0) {
       this.sessionEnd = true;
       //end this session
       //then upload everything in this session to database
-    } else {//handle the responsive: repeat some of the question
-      /*
-      if user is first time answering this question
-        if quality is easy, store info and end this right away
-        if quality is not easy, store info and repeat this three time
-      or if the user is attempting this question already
-        if quality is poor or repeat, repeat it once
-        if quality is good or easy, check if there are repeat time left
-          if no repeat time left, store info end this
-      */
-      var currentItem = this.qList.shift();//get the very first item
-      if (!currentItem.answered) {
-        currentItem.answered = true;//update currenItem answer
-        if (answer == 3) {//quality easy
-          //remove item from the list and
-          //to do: store the item
-        } else {
-          currentItem.level = answer;//this is the level
-          currentItem.repeatTime = 3;//repeat it for three times
-          this.qList.splice(Math.round(this.qList.length / 2), 0, currentItem);//insert this item to the middle
-        }
-      } else {
-        
-        if (answer == 0 || answer == 1) {
-          //don't change the repeat time
-          console.log('respond poor quality, repeat question')
-          this.qList.splice(Math.round(this.qList.length / 2), 0, currentItem);
-        } else {
-          if (currentItem.repeatTime == 1) {
-            //to do: store the item
-          } else {
-            console.log('respond good quality, minus repeat time by 1')
-            const repeatTime = currentItem.repeatTime;
-            currentItem.repeatTime = repeatTime - 1;
-            this.qList.splice(Math.round(this.qList.length / 2), 0, currentItem);
-          }
-        }
-      }
-
-
-      // this.index++;//update current number
-
-      this.displayAnswer = false;
-      // console.log('the user answered', answer);
-      this.updateEnableDisplayAnswer();
-
-      this.qList.forEach(e => {
-        console.log(e.id, 'repeat', e.repeatTime, 'level', e.level);
-      });
-      console.log('----------');
     }
+
+    // this.index++;//update current number
+
+    this.displayAnswer = false;
+    // console.log('the user answered', answer);
+    this.updateEnableDisplayAnswer();
+
+    this.qList.forEach(e => {
+      console.log(e.id, 'repeat', e.repeatTime, 'level', e.level);
+    });
+    console.log('----------');
+
     this.userCode = '';
     this.userMulti = '';
 
