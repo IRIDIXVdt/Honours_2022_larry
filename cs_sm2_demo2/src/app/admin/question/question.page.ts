17cf9300e988295d6afeeeece32cd910a1b9c985
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2022-08-26 21:41:23 +0800

    qa pair add edit and remove

--- cs_sm2_demo2/src/app/admin/question/question.page.ts
+++ cs_sm2_demo2/src/app/admin/question/question.page.ts
@@ -17,25 +17,15 @@ export class QuestionPage implements OnInit {
   public Editor = ClassicEditor;//ckeditor component
   public modelQuestion;//tracking question field
   public modelAnswer;//trakcing answer field
-  // @ViewChild('editorQuestion') editorQuestion: CKEditorComponent;
-  // @ViewChild('editorAnswer') editorAnswer: CKEditorComponent;
-  // @ViewChild('editorDescription') editorDescription: CKEditorComponent;
+
   @ViewChild('editorBody') editorBody: CKEditorComponent;
 
   public qType: string = 'df';//question type
   public qCourse: string = 'cosc304';//course type
   public qDes: string = '';
-  // public qQAP: QuestionAnswerPair[] = [{ question: '', answer: '' }];
+  public qQAP: QuestionAnswerPair[] = [{ question: '', answer: '' }];
 
-  public qQAP: QuestionAnswerPair[] = [
-    { question: 'oneq', answer: 'onea' },
-    { question: 'twoq', answer: 'twoa' }
-  ];
-
-  constructor(
-    private dt: DatabaseService,
-  ) {
-  }
+  constructor(private dt: DatabaseService) { }
 
   updateEditorField() {
     //update the question and answer datafield based on the current qType
@@ -44,17 +34,24 @@ export class QuestionPage implements OnInit {
     } else {
 
     }
-    // if (this.qType === "ba") {
-    //   this.editorQuestion.editorInstance.setData('<p>Term:&nbsp;</p>');
-    //   this.editorAnswer.editorInstance.setData('<p>Definition:&nbsp;</p>');
-    // } else if (this.qType === "mu") {
-    //   this.editorQuestion.editorInstance.setData('<p>Question:&nbsp;</p><p>&nbsp;</p><p>A):&nbsp;</p><p>B):&nbsp;</p><p>C):&nbsp;</p><p>D):&nbsp;</p><p>E):&nbsp;</p>');
-    //   this.editorAnswer.editorInstance.setData('<p>Correct Answer:&nbsp;</p><p>&nbsp;</p>');
-    // } else {
-    //   this.editorQuestion.editorInstance.setData('<p>Coding Question:&nbsp;</p>');
-    //   this.editorAnswer.editorInstance.setData('');
-    // }
-    // this.editorDescription.editorInstance.setData('<p>Description:&nbsp;</p>');
+    this.qaReset();
+  }
+
+  qaAdd() {
+    if (this.qType === 'df') {//only definition questions can have multiple qa pairs
+      const data: QuestionAnswerPair = { question: "", answer: "" };
+      this.qQAP.push(data);
+    }
+  }
+
+  qaRemove() {
+    if (this.qQAP.length > 1 && this.qType === 'df') {//never remove the last item
+      this.qQAP.pop();
+    }
+  }
+
+  qaReset() {
+    this.qQAP = [{ question: "new question", answer: "new answer" }];
   }
 
   ngOnInit() {
